198d7ea1262b68e3ea893c96d187a253b2ef6159
Vasilii Nikonov <nikonvasilii@gmail.com>
2026-05-13 20:13:14 -0700

    Add: the backbone of the TCP Server is complete

--- quote-server/src/main.rs
+++ quote-server/src/main.rs
@@ -1,3 +1,4 @@
+mod server;
 mod stock;
 
 fn main() {
---
+++ quote-server/src/server.rs
@@ -0,0 +1,70 @@
+use std::{
+    io::{BufRead, BufReader, Write},
+    net::TcpStream,
+    sync::{Arc, Mutex},
+};
+
+use crate::stock::StockMarket;
+
+pub fn handle_client(stream: TcpStream, market: Arc<Mutex<StockMarket>>) {
+    let mut writer = stream.try_clone().expect("failed to clone stream");
+    let mut reader = BufReader::new(stream);
+
+    let _ = writer.write_all(b"Welcome to the ypfinancialmarket. Your resource for being up to date with the latest financial information");
+    let _ = writer.flush();
+
+    let mut line = String::new();
+    loop {
+        line.clear();
+        match reader.read_line(&mut line) {
+            Ok(0) => {
+                return;
+            }
+            Ok(_) => {
+                let input = line.trim();
+                if input.is_empty() {
+                    let _ = writer.flush();
+                    continue;
+                }
+                let mut parts = input.split_whitespace();
+                let response = match parts.next() {
+                    Some("STREAM") => {
+                        let address = parts.next();
+                        let tickers = parts.next();
+
+                        if let (Some(address), Some(tickers)) = (address, tickers) {
+                            todo!()
+                        } else {
+                            "ERROR: usage STREAM udp://127.0.0.1:<port_number> <tick1>,<tick2>,...,<tick[n]>\n".to_string()
+                        }
+                    }
+                    Some("GET") => {
+                        todo!()
+                    }
+                    Some("GET_MANY") => {
+                        todo!()
+                    }
+                    Some("LIST") => {
+                        todo!()
+                    }
+                    Some("PING") => {
+                        todo!()
+                    }
+                    Some("CONNECTIONS") => {
+                        todo!()
+                    }
+                    Some("HELP") => {
+                        todo!()
+                    }
+                    _ => "ERROR: Unknown command\n".to_string(),
+                };
+
+                let _ = writer.write_all(response.as_bytes());
+                let _ = writer.flush();
+            }
+            Err(_) => {
+                return;
+            }
+        }
+    }
+}
